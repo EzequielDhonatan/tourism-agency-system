be1efdab2de67674f4a66c0181cb7084320f542e
Ezequiel Dhonatan <Ezequieldhonatan@gmail.com>
2019-09-11 20:21:19 -0300

    Module 3.14 - Improvements

--- resources/views/panel/brands/search.blade.php
+++ resources/views/panel/brands/search.blade.php
@@ -11,7 +11,7 @@
 @if (isset($dataForm['key_search']))
     <div class="alert alert-info">
         <p>
-            <a href="{{route('brands.index')}}">
+            <a href="{{ route('brands.index') }}">
                 <i class="fa fa-refresh" aria-hidden="true"></i>
             </a>
             Pesquisado: <strong>{{ $dataForm['key_search'] }}</strong>
